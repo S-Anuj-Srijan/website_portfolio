5f41d0d0fbd64dd1027dd21f8a57cd30a688dc55
S-Anuj-Srijan <Anuj.Srijan.S@gmail.com>
2025-05-11 22:27:55 +0530

    i think its done if not i will be sad

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,6 @@ import react from '@vitejs/plugin-react'; // or vue or svelte, etc.
 
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/website_portfolio",
-  build: {
-    chunkSizeWarningLimit: 1000, // Increase from default 500kB to 1000kB
-
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor-react'; // Or vue, svelte, etc.
-            if (id.includes('lodash')) return 'vendor-lodash'; // Example
-            return 'vendor'; // Default vendor chunk
-          }
-        }
-      }
-    }
-  }
+  base:"/"
+  
 });
